fed7b21151177fce31bb38f9c8a7bc044025b31f
Andrés <andresff@alumnos.iesgalileo.es>
2022-01-17 15:44:24 +0100

    Primer ejercicio real con Vue cli

---
+++ 09-Vue-CLI/proyecto-2/src/components/Lista.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <ul class="list-group">
+            <li v-for="(item, index) of arryOrdenado" :key="item.id" @click="aumentar(index)" 
+            class="list-group-item d-flex justify-content-between align-items-center">
+                {{index}} - {{item.nombre}}
+                <span class="badge bg-primary rounded-pill">{{item.cantidad}}</span>
+            </li>
+        </ul>
+        <button class="btn btn-danger btn-block" @click="reiniciar">Reiniciar</button>
+    </div>
+</template>
+
+<script>
+
+    import {mapState, mapMutations} from 'vuex';
+
+    export default{
+        name:'Lista',
+        computed:{
+            ...mapState(['frutas']),
+            arryOrdenado(){
+                return this.frutas.sort( (a, b) =>b.cantidad - a.cantidad)
+            }
+        },
+        methods: {
+            ...mapMutations(['aumentar','reiniciar'])
+        }
+
+    }
+</script>
