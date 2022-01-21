88c5e33497424ff8deaafb64b833f416f30ae568
Andrés <andresff@alumnos.iesgalileo.es>
2022-01-21 14:52:12 +0100

    Dialog o modales con Vuetify

--- 10-Vuetify-Axios/vuetify-youtube/src/views/Home.vue
+++ 10-Vuetify-Axios/vuetify-youtube/src/views/Home.vue
@@ -38,16 +38,39 @@
             aperiam ipsum iusto aliquam molestiae expedita dolore eveniet inventore laboriosam.</p>
           </v-card-text>
           <v-card-actions>
-            <v-btn color="info">Accion</v-btn>
+            <v-btn color="info" @click="dialog = true">Accion</v-btn>
           </v-card-actions>
         </v-card>
       </v-flex>
     </v-layout>
+
+    <!-- DIALOG -->
+    <v-dialog v-model="dialog" width="500px" persistent>
+      <v-card>
+        <v-card-title>Este es el Titulo</v-card-title>
+        <v-card-text>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+          Incidunt necessitatibus ratione mollitia provident pariatur, 
+          maxime voluptate asperiores quibusdam odio veniam beatae illo fuga 
+          quam quas eligendi sequi saepe porro officia!</v-card-text>
+          <v-divider></v-divider>
+          <v-actions>
+            <v-btn @click="dialog = false">Cerrar</v-btn>
+          </v-actions>
+      </v-card>
+    </v-dialog>
+
+    <!-- FIN DIALOG -->
+
   </v-container>
 </template>
 
 <script>
 
   export default {
+    data(){
+      return{
+        dialog: false
+      }
+    }
   }
 </script>
